49d7da951cd53670b4b30d017088395225dd86f0
filip.lunski <filip.lunski@profiq.com>
2023-11-13 15:10:31 +0100

    Refactor code for improved readability and performance.

--- app/api/lib/result.ts
+++ app/api/lib/result.ts
@@ -1,34 +1,11 @@
 "use server";
 import fs from "fs";
 
-const allTechOptions: { [key: string]: string } = require("@/techOptions.json");
-const techOptionsFiles: {
-  [key: string]: string[];
-} = require("@/techOptionsFiles.json");
+import techOptions from "@/techOptions.json";
+import techOptFiles from "@/techOptionsFiles.json";
 
-/**
- * Retrieves the gitignore template for a given technology option.
- * @param techOption - The name of the technology option to retrieve the gitignore template for.
- * @returns A string containing the gitignore template for the specified technology option.
- */
-async function getElementarResult(techOption: string): Promise<string> {
-  // console.log(encodeURI(techOption))
-  try {
-    // console.log(techOption);
-    // Get the name of the technology option
-    let techOptionName: string = allTechOptions[techOption.toLowerCase()];
-    // Read the proper file in the directory
-    // console.log(await fs.promises.readdir('./'))
-    let resultText: string = await fs.promises.readFile(
-      `./toptal.gitignoreTemplates/gitignore/templates/${techOptionName}.gitignore`,
-      "utf8",
-    );
-    return `### ${techOptionName} ###\n` + resultText;
-  } catch (e) {
-    // console.log(e);
-    return `### ${techOption} ###\n#!# No results found #!#`;
-  }
-}
+const techOptionsFiles: { [key: string]: string[] } = techOptFiles;
+const allTechOptions: { [key: string]: string } = techOptions;
 
 /**
  * Retrieves the gitignore templates for multiple technology options.
@@ -37,52 +14,57 @@ async function getElementarResult(techOption: string): Promise<string> {
  */
 export async function getResult(techOptions: string[]): Promise<string> {
   let filesDict: { [key: string]: string[] } = {};
+
+  // creating a dictionary of all needed files and their corresponding tech options
   techOptions.forEach((option) => {
     techOptionsFiles[allTechOptions[option.toLowerCase()]].map((file) => {
       filesDict[file] = filesDict[file] || [];
       filesDict[file].push(option);
     });
   });
+
+  // reads all needed files and creates a single string
   let result = (
     await Promise.all(
       Object.keys(filesDict).map(async (file) => {
+        // reading the file
         let resultText: string = await fs.promises.readFile(
           `./toptal.gitignoreTemplates/gitignore/templates/${file}`,
           "utf8",
         );
+        // adding header with the file name
         let result = `## file: ${file} ##\n\n${resultText}`;
+        // if the file is a patch, return it as is
         if (file.endsWith("patch")) {
           return result;
         }
+        // if the file is not a patch, add the tech options names to the header
+        // if the file hasn't the same name as option but there is a file with the option name then it is a stack file
+        // and we add the stack name to the header
         let optionNames = filesDict[file].map((option) => {
           if (
-            option + ".gitignore" == file ||
-            !Object.keys(filesDict).includes(option + ".gitignore")
+            option + ".gitignore" != file &&
+            Object.keys(filesDict).includes(option + ".gitignore")
           ) {
-            return option;
+            return `${option}.${file.slice(0, file.indexOf("."))} Stack`;
           }
-          return `${option}.${file.slice(0, file.indexOf("."))}`;
+          return option;
         });
         return `### ${optionNames.join(", ")} ###\n${result}`;
       }),
     )
-  ).join("\n\n");
+  )
+    // joining all files into a single string with two empty lines between each file
+    .join("\n\n");
 
+  // commenting all duplicate rules
   result = result
     .split("\n")
     .map((line, index, array) => {
-      if (array.indexOf(line) === index) return line;
+      if (array.indexOf(line) === index || line.length == 0) return line;
       return `#${line} #duplicate rule`;
     })
     .join("\n");
 
   return result;
-  // Get the results for each option
-  let results = await Promise.all(
-    techOptions.map(async (option) => getElementarResult(option)),
-  );
-  // Join the results into a single string
-  return results.join(
-    "\n-----------------------------------------------------------------------------------------------------------------------\n\n",
-  );
 }
