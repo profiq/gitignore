6156963aa8c2c204112cd61e2b408aeff999abe3
FilipLunski <filip.lunski@profiq.com>
2023-11-27 13:18:37 +0100

    deploing to edgio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+const { withEdgio } = require("@edgio/next/config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const _preEdgioExport = nextConfig;
+
+module.exports = (phase, config) =>
+  withEdgio({
+    ..._preEdgioExport,
+  });
