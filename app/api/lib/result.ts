1a14f156f86c6ecb6ab5acb101a75d065c7b92c7
FilipLunski <filip.lunski@profiq.com>
2023-12-11 10:29:26 +0100

    tests for result: createNeededFilesDict, fileHeader

--- app/api/lib/result.ts
+++ app/api/lib/result.ts
@@ -11,12 +11,59 @@ export async function removeDuplicates(arr: string): Promise<string> {
   return arr
     .split("\n")
     .map((line, index, array) => {
-      console.log(index+line)
       if (array.indexOf(line) === index || line.length === 0) return line;
       return `#${line} #duplicate rule`;
     })
     .join("\n");
 }
+
+/**
+ * Creates a dictionary of technology gitignore templates and their corresponding options.
+ * @param techOptions - An array of requested technology options to retrieve the gitignore templates for.
+ * @returns A dictionary of gitignore templates files names and their corresponding options.
+ */
+export async function createNeededFilesDict(techOptions: string[], allTechOptions: { [key: string]: string }, techOptionsFiles: { [key: string]: string[] }): Promise<{ [key: string]: string[] }> {
+  const filesDict: { [key: string]: string[] } = {};
+
+  techOptions.forEach((option) => {
+    techOptionsFiles[allTechOptions[option.toLowerCase()]].map((file) => {
+      filesDict[file] = filesDict[file] || [];
+      filesDict[file].push(option);
+    });
+  });
+
+  return filesDict;
+}
+
+/**
+ * creates a header for the gitignore file based on corresponding options.
+ * @param file - The name of the gitignore file.
+ * @param filesDict - A dictionary of gitignore file names and their corresponding options.
+ * @returns A string containing the header for the gitignore file.
+ */
+export async function fileHeader(file: string, filesDict: { [key: string]: string[] }): Promise<string> {
+
+  let options = "";
+
+  // if the file is not a patch, add the tech options names to the header
+  // if the file hasn't the same name as option but there is a file with the option name
+  // then it is a stack file and we add the stack name to the header
+  if (!file.endsWith("patch")) {
+    let optionNames = filesDict[file].map((option) => {
+      if (
+        option + ".gitignore" != file &&
+        Object.keys(filesDict).includes(option + ".gitignore")
+      ) {
+        return `${option}.${file.slice(0, file.indexOf("."))} Stack`;
+      }
+      return option;
+    });
+    options = `### ${optionNames.join(", ")} ###\n`
+  }
+
+  return `${options}## file: ${file} ##\n\n`
+}
+
 
 /**
  * Retrieves the gitignore templates for multiple technology options.
@@ -27,15 +74,8 @@ export async function getResult(
   techOptions: string[],
   remDupl: boolean = true,
 ): Promise<string> {
-  let filesDict: { [key: string]: string[] } = {};
 
-  // creating a dictionary of all needed files and their corresponding tech options
-  techOptions.forEach((option) => {
-    techOptionsFiles[allTechOptions[option.toLowerCase()]].map((file) => {
-      filesDict[file] = filesDict[file] || [];
-      filesDict[file].push(option);
-    });
-  });
+  const filesDict = await createNeededFilesDict(techOptions, allTechOptions, techOptionsFiles);
 
   // reads all needed files and creates a single string
   let result = (
@@ -46,27 +86,9 @@ export async function getResult(
           `./toptal.gitignoreTemplates/templates/${file}`,
           "utf8",
         );
-        // adding header with the file name
-        let result = `## file: ${file} ##\n\n${resultText}`;
-        // if the file is a patch, return it as is
-        if (file.endsWith("patch")) {
-          return result;
-        }
-        // if the file is not a patch, add the tech options names to the header
-        // if the file hasn't the same name as option but there is a file with the option name then it is a stack file
-        // and we add the stack name to the header
-        let optionNames = filesDict[file].map((option) => {
-          if (
-            option + ".gitignore" != file &&
-            Object.keys(filesDict).includes(option + ".gitignore")
-          ) {
-            return `${option}.${file.slice(0, file.indexOf("."))} Stack`;
-          }
-          return option;
-        });
-        return `### ${optionNames.join(", ")} ###\n${result}`;
-      }),
-    )
+
+        return `${await fileHeader(file, filesDict)}${resultText}`;
+      }))
   )
     // joining all files into a single string with two empty lines between each file
     .join("\n\n");
--- tests/result.test.ts
+++ tests/result.test.ts
@@ -1,10 +1,134 @@
-const { removeDuplicates } = require("../app/api/lib/result");
+import {
+  removeDuplicates,
+  createNeededFilesDict,
+  fileHeader,
+} from "../app/api/lib/result";
 import fs from "fs";
 
+import techOptions from "./techOptions.json";
+import techOptFiles from "./techOptionsFiles.json";
+
+const techOptionsFiles: { [key: string]: string[] } = techOptFiles;
+const allTechOptions: { [key: string]: string } = techOptions;
+
+// 1. removeDuplicates
 test("remove (comment) duplicate lines", async () => {
   const input = fs.readFileSync("./tests/remDupl_input.gitignore", "utf8");
-
   const output = fs.readFileSync("./tests/remDupl_output.gitignore", "utf8");
-
   expect(await removeDuplicates(input)).toBe(output);
+});
+
+//-------------------------------------------------------------------------------
+
+// 2. createNeededFilesDict
+const sortArrays = (obj: {
+  [key: string]: string[];
+}): { [key: string]: string[] } => {
+  const sortedObj: { [key: string]: string[] } = {};
+
+  Object.keys(obj)
+    .sort()
+    .forEach((key) => {
+      sortedObj[key] = obj[key].sort();
+    });
+  return sortedObj;
+};
+
+test("create needed files dictionary: c, c++", async () => {
+  const techOptions = ["c++", "c"];
+  const expectedDict = {
+    "C++.gitignore": ["c++"],
+    "C.gitignore": ["c"],
+  };
+  const result = await createNeededFilesDict(
+    techOptions,
+    allTechOptions,
+    techOptionsFiles,
+  );
+  expect(result).toEqual(expectedDict);
+});
+
+test("create needed files dictionary: react, android", async () => {
+  const techOptions = ["reactnative", "android", "Gradle"];
+  const expectedDict = {
+    "Android.gitignore": ["android", "reactnative"],
+    "Android.patch": ["reactnative", "android"],
+    "Buck.gitignore": ["reactnative"],
+    "Gradle.gitignore": ["reactnative", "Gradle"],
+    "Gradle.patch": ["Gradle", "reactnative"],
+    "Linux.gitignore": ["reactnative"],
+    "Node.gitignore": ["reactnative"],
+    "Node.patch": ["reactnative"],
+    "ReactNative.gitignore": ["reactnative"],
+    "Xcode.gitignore": ["reactnative"],
+    "Xcode.patch": ["reactnative"],
+    "macOS.gitignore": ["reactnative"],
+    "macOS.patch": ["reactnative"],
+  };
+
+  const result = await createNeededFilesDict(
+    techOptions,
+    allTechOptions,
+    techOptionsFiles,
+  );
+
+  expect(sortArrays(result)).toEqual(sortArrays(expectedDict));
+});
+
+//-------------------------------------------------------------------------------
+
+// 3. fileHeader
+
+let filesDict = {
+  "ReactNative.gitignore": ["ReactNative"],
+  "Android.gitignore": ["ReactNative", "Android"],
+  "Android.patch": ["ReactNative", "Android"],
+  "Buck.gitignore": ["ReactNative"],
+  "Gradle.gitignore": ["ReactNative", "Gradle"],
+  "Gradle.patch": ["ReactNative", "Gradle"],
+  "Linux.gitignore": ["ReactNative"],
+  "macOS.gitignore": ["ReactNative"],
+  "macOS.patch": ["ReactNative"],
+  "Node.gitignore": ["ReactNative"],
+  "Node.patch": ["ReactNative"],
+  "Xcode.gitignore": ["ReactNative"],
+  "Xcode.patch": ["ReactNative"],
+};
+
+test("generate file header: ReactNative.gitignore", async () => {
+  const file = "ReactNative.gitignore";
+
+  const result = await fileHeader(file, sortArrays(filesDict));
+
+  const expectedHeader = `### ReactNative ###
+## file: ReactNative.gitignore ##
+
+`;
+
+  expect(result).toBe(expectedHeader);
+});
+
+test("generate file header: Gradle.gitignore", async () => {
+  const file = "Gradle.gitignore";
+
+  const result = await fileHeader(file, sortArrays(filesDict));
+
+  const expectedHeader = `### Gradle, ReactNative.Gradle Stack ###
+## file: Gradle.gitignore ##
+
+`;
+
+  expect(result).toBe(expectedHeader);
+});
+
+test("generate file header: Gradle.patch", async () => {
+  const file = "Gradle.patch";
+
+  const result = await fileHeader(file, sortArrays(filesDict));
+
+  const expectedHeader = `## file: Gradle.patch ##
+
+`;
+
+  expect(result).toBe(expectedHeader);
 });
