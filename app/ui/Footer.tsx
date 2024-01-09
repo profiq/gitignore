b767a3330f3385cb372615c5b46f5a8f0207fcc3
Dušan Kováčik <dusan.kovacik@profiq.com>
2024-01-09 09:18:36 +0100

    Fix footer label

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -10,10 +10,11 @@ import { clsx } from "clsx";
 const Footer: React.FC = () => {
   return (
     <footer className={clsx("z-50 bg-profiq-blue h-20 ")}>
-      <div className={clsx("max-w-5xl mx-auto px-5 ")}>
+      <div className="flex flex-row justify-end items-center space-x-4 max-w-5xl mx-auto px-5">
+        <div className="text-profiq-white text-lg">Powered by</div>
         <a
           className={clsx(
-            "h-20 w-36 block float-right my-0 mx-auto",
+            "h-20 w-36 block my-0",
             "bg-profiq-logo hover:bg-profiq-logo-hover bg-no-repeat bg-center bg-contain",
           )}
           href="https://www.profiq.com/"
